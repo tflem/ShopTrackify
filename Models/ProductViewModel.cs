03c926e6b49263d1dbdb6fcfdd5feeb913c4eebc
tfleming <tfleming26@hotmail.com>
2018-04-28 03:03:03 -0600

    Add product view model

---
+++ Models/ProductViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ShopTrackify.Models
+{
+    public class ProductViewModel
+    {
+        public IEnumerable<ProductItem> Products { get; set; }
+    }
+}

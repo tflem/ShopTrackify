909b2a0afc8a5d8ca2e21f5d010c59ee11708c55
tfleming <tfleming26@hotmail.com>
2018-04-28 02:28:25 -0600

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShopTrackify.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# ShopTrackify
+
+> A shop tracker application, which features or will feature the following:
+
+* Dotnet Core MVC
+* HTML
+* CSS
+* jQuery
+* SQL Server
+* Bootstrap
+* Entity Framework Core

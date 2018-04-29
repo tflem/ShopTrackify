b9796eed6c855ec086fccab548d19e9661dbf20d
tfleming <tfleming26@hotmail.com>
2018-04-29 12:30:06 -0600

    Create product item interface

---
+++ Services/IProductItemService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ShopTrackify.Models;
+
+namespace ShopTrackify.Services
+{
+    public interface IProductItemService
+    {
+        Task<IEnumerable<ProductItem>> GetRemainingProductsAsync();
+    }
+}

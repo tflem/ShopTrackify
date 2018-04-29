964d79c222e6e723bed3308c975af6b1c3d90cc0
tfleming <tfleming26@hotmail.com>
2018-04-28 18:26:29 -0600

    Add dbset property for productitem entity

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -15,6 +15,8 @@ namespace ShopTrackify.Data
         {
         }
 
+        public DbSet<ProductItem> Products { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);

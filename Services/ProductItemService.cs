80991e6fd9b75f14059fd5cefd84cf7d59bc5cae
tfleming <tfleming26@hotmail.com>
2018-04-29 12:32:48 -0600

    Add new service class

---
+++ Services/ProductItemService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShopTrackify.Data;
+using ShopTrackify.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShopTrackify.Services
+{
+    public class ProductItemService : IProductItemService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductItemService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ProductItem>> GetRemainingProductsAsync()
+        {
+            var products = await _context.Products
+                .Where(x => x.Id != null)
+                .ToArrayAsync();
+
+            return products;
+        }
+    }
+}
